4da855d35b329be0a6885cd67cafca2d2831fe40
Andrei_Rohau <rohau.andrei@gmail.com>
2022-09-05 05:31:51 +0300

    working solution t2 (removes explicit headers from Q) need refactoring

--- src/main/java/org/learn/config/JmsConfig.java
+++ src/main/java/org/learn/config/JmsConfig.java
@@ -3,7 +3,10 @@ package org.learn.config;
 import lombok.extern.slf4j.Slf4j;
 import org.learn.jms.CustomConsumer;
 import org.learn.jms.FailedMessageProcessor;
+import org.springframework.amqp.core.DeclarableCustomizer;
+import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
@@ -21,6 +24,23 @@ public class JmsConfig {
 //        log.info("_____errorChannel Handling ERROR: " + message);
 //        failedMessageProcessor.processFailed(message);
 //    }
+
+    // wellll, I'm probably need to open a jira ticket in spring-cloud-steam-rabbitmq repository
+    @Bean
+    @ConditionalOnProperty(name = "republish", havingValue = "true")
+    public DeclarableCustomizer declarableCustomizer() {
+        return declarable -> {
+            if (declarable instanceof Queue) {
+                var queue = (Queue) declarable;
+                if (queue.getName().equals("demo-queue-1")
+                        || queue.getName().equals("demo-queue-2")) {
+                    queue.removeArgument("x-dead-letter-exchange");
+                    queue.removeArgument("x-dead-letter-routing-key");
+                }
+            }
+            return declarable;
+        };
+    }
 
     @Bean
     public Consumer<Message<String>> queue1Sink() {
